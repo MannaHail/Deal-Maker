a68e7e888f2a8715726b1a72bf07ba132703d145
MannaHail <mannahail42@gmail.com>
2022-09-28 13:16:58 -0400

    Add project files.

---
+++ The Deal Maker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Character {
+    string name;
+    unsigned int charisma;
+    class Player {
+        unsigned int influence;
+        unsigned int souls;
+    };
+    class Npc {
+        unsigned int greed;
+        unsigned int caution;
+    };
+};
+
+
+int main()
+{
+    
+
+    system("pause>0");
+}
+
